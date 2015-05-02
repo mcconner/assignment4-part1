223588be7bc1dbc2cda1e71c43488bcdfd1c9db1
Rachael McConnell <mcconner@onid.oregonstate.edu>
2015-05-01 22:12:46 -0700

    Added code to make it so that a user cannot get to this page unless they are logged in

--- src/content2.php
+++ src/content2.php
@@ -1,28 +1,23 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 session_start();
-if(! isset($_POST['username'])){
-	$_SESSION = array();
-	session_destroy();
+
+//if(session_status() !== PHP_SESSION_ACTIVE || $_SESSION['username'] == ''){
+//if(session_status() !== PHP_SESSION_ACTIVE){
+if(!isset($_SESSION['username'])){
 	$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
 	$filePath = implode('/', $filePath);
 	$redirect = "//" . $_SERVER['HTTP_HOST'] . $filePath;
 	header("location: {$redirect}/login.php", true);
-	die();
 }
 
-if(session_status() == PHP_SESSION_ACTIVE){	
-	if(isset($_POST['username'])){
-		$_SESSION['username'] = $_POST['username'];
-	}
-	
-	if(!isset($_SESSION['visits'])){
-		$_SESSION['visits'] = 0;
-	}
-	$_SESSION['visits']++;
-	echo "Hi " . $_SESSION['username'] . ", you have visited this page " . $_SESSION['visits'] . " times. \n";
-}
+echo "session status: " . session_status();
+//echo "session username: " . $_SESSION['username'];
+echo "Hello $_SESSION[username] <br>";
+echo "<h2>Content2.php</h2>";
+
+echo "Click <a href = 'http://web.engr.oregonstate.edu/~mcconner/content1.php'> here</a> to go to content1.php.";
+
 ?>
 
