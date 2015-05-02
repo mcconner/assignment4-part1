597172e6a91cad5e31b6ce3091e9bc386aae405f
Rachael McConnell <mcconner@onid.oregonstate.edu>
2015-05-01 22:12:19 -0700

    Moved code to destroy a session to the login page

--- src/login.php
+++ src/login.php
@@ -1,3 +1,30 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+
+//$loggedIn = 1;
+$strAction = '';
+if(isset($_GET['action']))
+	$strAction = $_GET['action'];
+
+//if(isset($_GET['action']) && $_GET['action'] == 'logout'){
+if($strAction == 'logout'){
+	$_SESSION = array();
+	session_destroy();
+	//$loggedIn = 0;
+	$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+	$filePath = implode('/', $filePath);
+	$redirect = "//" . $_SERVER['HTTP_HOST'] . $filePath;
+	header("location: {$redirect}/login.php", true);
+	die();
+}
+//echo "Logged in= " . $loggedIn;
+echo "strAction = " . $strAction;
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +33,8 @@
 </head>
 <body>
 
-   
-<form  name='login' action='content2.php' method='POST'> 
+<h3>Welcome, please login </h2>   
+<form  name='login' action='content1.php' method='POST'> 
 <label>Username: </label>
 <input type='text' name='username'>
 <input type='submit' name='login' value='login'>
